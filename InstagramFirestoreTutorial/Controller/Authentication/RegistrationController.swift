0f8d0eb01b03b69d7406ae0fe3ede50d3e0084d7
doyeon <doyeon326@gmail.com>
2021-03-11 19:54:08 +0900

    feat: Login Page UI

---
+++ InstagramFirestoreTutorial/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,21 @@
+//
+//  RegistrationController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Tony Jung on 2021/03/11.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // Mark: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
