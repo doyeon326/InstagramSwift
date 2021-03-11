a99e3ae6ecb392a2616a5f1368fbb39f12c228c3
doyeon <doyeon326@gmail.com>
2021-03-11 16:40:06 +0900

    set up rootView

---
+++ InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Tony Jung on 2021/03/11.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
